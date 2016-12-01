881357725a10e07917af810127697f6ae60391d0
Jeremy S. Postelnek <jeremy.postelnek@gmail.com>
2016-12-01 18:57:50 -0500

    make life easy with this script

---
+++ setup.sh
@@ -0,0 +1,26 @@
+# Yeah so this is a bash script and it'll set up a lot of the stuff I need.
+# All you'll need to do is do chmod +x ./setup.sh and run it from terminal
+
+# i3 config
+mv .config ~/
+
+# fonts used
+mv .fonts ~/
+
+# backgrounds
+mv background ~/
+
+# .bashrc
+mv .bashrc ~/
+
+#gtk-2.0 thing
+mv .gtkrc-2.0 ~/
+
+pacman -S i3 i3-gaps i3lock-fancy-git rxvt-unicode luarocks5.1 obs-studio spotify \
+  discord-canary firefox dropbox libreoffice-fresh lxappearance nautilus git \
+  audacity tiled visual-studio-code love jdk7-open-infinality gtk-theme-arc-git \
+  paper-icon-theme-git
+
+# Install moontastic terminal theme
+# Thanks VideahGams, you're awesome, fam!
+sudo luarocks-5.1 install moontastic
