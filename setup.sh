3fae7b0d6b1b562648ef677983a59dcb3d68b8cc
Jeremy S. Postelnek <jeremy.postelnek@gmail.com>
2016-12-03 23:49:05 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,8 +22,9 @@ xrdb -merge ~/.Xresources
 
 pacman -S i3 i3-gaps i3lock-fancy-git feh rxvt-unicode luarocks5.1 obs-studio spotify \
   discord-canary google-chrome-stable dropbox libreoffice-fresh lxappearance nautilus git \
-  audacity tiled visual-studio-code love jdk7-open-infinality gtk-theme-arc-git \
-  paper-icon-theme-git dunst compton
+  audacity tiled visual-studio-code love jdk8-openjdk-infinality gtk-theme-arc-git \
+  paper-icon-theme-git dunst compton freetype2-infinality fontconfig-infinality \
+  
 
 # Install moontastic terminal theme
 # Thanks VideahGams, you're awesome, fam!
